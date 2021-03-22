43cb441d2ab3238dd4f08e79715d332621fa2fa2
kmarion76 <47949532+kmarion76@users.noreply.github.com>
2021-03-22 18:34:20 -0400

    front end assignment

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+
+  $(function () {
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    // AOS ANIMATION
+    AOS.init({
+      disable: 'mobile',
+      duration: 800,
+      anchorPlacement: 'center-bottom'
+    });
+
+
+    // SMOOTHSCROLL NAVBAR
+    $(function() {
+      $('.navbar a, .hero-text a').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });    
+  });
+  function ValidateEmail() 
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(cf-email))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+function validateName()
+{
+var name = document.myform.personname.value;
+if (name < 6)
+{
+alert("Username must be atleast 6 charactrs long, Please Try Again");
+}
+
+}
+
+
+    
+
